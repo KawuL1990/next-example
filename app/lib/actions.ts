544c4f54a9d50753659b215cedbef7cbd01d98f1
oleksii.dombrovskyi <aleksey.dombrovskiiy@gmail.com>
2025-01-18 23:39:30 +0100

    fix error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -89,7 +89,7 @@ export async function updateInvoice(id: string, formData: FormData) {
   redirect("/dashboard/invoices");
 }
 
-export async function deleteInvoice(id: string) {
+export async function deleteInvoice(id: string): Promise<{ message: string }> {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
